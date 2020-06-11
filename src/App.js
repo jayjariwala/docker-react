e195d7fc6e72c75f546fbf9716b3fd67e4745c82
jayjariwala <jay.jariwala@hotmail.com>
2020-06-11 13:27:58 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with me?
-          <h1> Hello world</h1>
+          <h2>I was changed on the feature branch</h2>
         </a>
       </header>
     </div>
